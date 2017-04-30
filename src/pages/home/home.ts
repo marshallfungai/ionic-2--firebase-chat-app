590b8fb372c6aa3ad522e2487c643e1e436838fa
kmarshy <fmarshall.m@gmail.com>
2017-04-30 14:48:14 +0300

    changes to be committed: 	modified:   package.json 	modified:   src/index.html 	modified:   src/pages/home/home.html 	modified:   src/pages/home/home.ts  next is styling

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Injectable} from "angular2/core";
 import { NavController } from 'ionic-angular';
 import { AngularFire, AuthProviders, AuthMethods,FirebaseListObservable } from 'angularfire2';
 
